e6ffbe6779a25ab42e2db3c8d8ec3275948fad51
qiyunfengqiyunfeng <120168773+qiyunfengqiyunfeng@users.noreply.github.com>
2022-12-12 00:36:41 +0800

    Update GitTest.java

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println(123);
         System.out.println("push_test");
+        System.out.println("poll_test");
     }
 }
